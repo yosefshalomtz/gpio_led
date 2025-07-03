3e3e80b276599f1aa5873c35614339fa560eb1dd
Yosef <yosefshalomtz@gmail.com>
2025-07-04 00:11:35 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// Simple LED blink for Raspberry Pi Zero W using WiringPi
+#include <wiringPi.h>
+#include <iostream>
+#include <unistd.h> // for sleep
+
+#define LED_PIN 0 // WiringPi pin 0 = BCM_GPIO 17 (Physical pin 11)
+
+int main() {
+    // Initialize WiringPi using BCM_GPIO numbering
+    if (wiringPiSetup() == -1) {
+        std::cerr << "WiringPi setup failed!\n";
+        return 1;
+    }
+
+    pinMode(LED_PIN, OUTPUT);
+
+    std::cout << "Blinking LED on GPIO 17 (WiringPi pin 0)... Press Ctrl+C to stop.\n";
+    while (true) {
+        digitalWrite(LED_PIN, HIGH); // LED on
+        delay(500);                 // 500 ms
+        digitalWrite(LED_PIN, LOW);  // LED off
+        delay(500);                 // 500 ms
+    }
+    return 0;
+}
